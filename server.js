9df1eb1914c79ce10620dd55b389fa054493dc6b
adhi24-R <adhi12200524@gmail.com>
2025-12-02 17:11:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get('/config', (req, res) => {
 
 // Serve the main HTML file
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // Start Server
